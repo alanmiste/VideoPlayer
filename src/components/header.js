dea8e6e6928a18a39985962165954ee1ee1ae953
alanmiste <baranalaan@gmail.com>
2021-09-27 16:06:05 +0200

    add button ande header .js

---
+++ src/components/header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header(props){
+    return(
+        <header className={props.classText}>
+            <p>Simple video player</p>
+        </header>
+    )
+}
