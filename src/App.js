333d702e4b895380c8290694d15dec6e4b1fe146
alanmiste <baranalaan@gmail.com>
2021-09-27 16:09:59 +0200

    edit App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import Screen from './components/videoScreen'
+import Header from './components/header'
+import Button from './components/button';
+import { FiPlay } from 'react-icons/fi';
+import { FaStop } from 'react-icons/fa';
+import { FiPause } from 'react-icons/fi';
+import { FiFastForward } from 'react-icons/fi';
+import { FiRewind } from 'react-icons/fi';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    pp: false
+  }
+
+  playPause= () => {
+    const hh = this.state.pp;
+    this.setState({pp: !hh })
+    
+  }
+
+
+  render() {
+    return (
+      <div>
+        <Header classText='header' />
+        <Screen classText='screen' />
+        <div className='main'>
+          <Button classText='controlBtn' fontIcon={<FaStop />} />
+          {
+            !this.state.pp ? <Button classText='controlBtn' cb={this.playPause} fontIcon={<FiPlay />} />
+              :
+              <Button classText='controlBtn' cb={this.playPause} fontIcon={<FiPause />} />
+          }
+          <Button classText='controlBtn' fontIcon={<FiRewind />} />
+          <Button classText='controlBtn' fontIcon={<FiFastForward />} />
+        </div>
+      </div>
+
+    );
+  }
 }
+
 
 export default App;
